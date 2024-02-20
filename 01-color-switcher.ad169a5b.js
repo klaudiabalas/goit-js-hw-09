0ed29ad70f828406c97db75d60e4e283cd61e21c
klaudiabalas <103740572+klaudiabalas@users.noreply.github.com>
2024-02-20 13:19:45 +0000

    Deploying to gh-pages from @ klaudiabalas/goit-js-hw-09@9f8f64eeeee192c70d791fe4113a54c1145b6c52 🚀

--- 01-color-switcher.ad169a5b.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body"),n=null;e.disabled=!0,t.addEventListener("click",(function(){n=setInterval((function(){color="#".concat(Math.floor(16777215*Math.random()).toString(16)),o.style.backgroundColor=color}),1e3),e.disabled=!1,t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.ad169a5b.js.map
